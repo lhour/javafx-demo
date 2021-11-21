82771beebd40658b870f5e34c180c1dee9057cc7
lhour <65876229+lhour@users.noreply.github.com>
2021-11-21 11:26:51 +0800

    优化了 计算器控制器 的类

--- src/main/java/com/hour/controller/CalculatorController.java
+++ src/main/java/com/hour/controller/CalculatorController.java
@@ -17,19 +17,37 @@ import java.util.ArrayList;
 
 public class CalculatorController extends HomeController {
 
-    @FXML
-    public TextField show;
+    public final static int NUMBER_Point = -1;
+    public final static int NUMBER_0 = 0;
+    public final static int NUMBER_1 = 1;
+    public final static int NUMBER_2 = 2;
+    public final static int NUMBER_3 = 3;
+    public final static int NUMBER_4 = 4;
+    public final static int NUMBER_5 = 5;
+    public final static int NUMBER_6 = 6;
+    public final static int NUMBER_7 = 7;
+    public final static int NUMBER_8 = 8;
+    public final static int NUMBER_9 = 9;
+    public final static int NUMBER_a = 10;
+    public final static int NUMBER_b = 11;
+    public final static int NUMBER_c = 12;
+    public final static int NUMBER_d = 13;
+    public final static int NUMBER_e = 14;
+    public final static int NUMBER_f = 15;
+
+    public final static int SYMBLE_REMAINDER = 0;  // 求余
+    public final static int SYMBLE_POW = 1;        // 乘方
+    public final static int SYMBLE_SQRT = 2;       // 开根
+    public final static int SYMBLE_ADD = 3;        // 加
+    public final static int SYMBLE_SUBSTRUCT = 4;  //减
+    public final static int SYMBLE_MULTIPLY = 5;   //乘
+    public final static int SYMBLE_DIVIDE = 6;     //除
+
+    public final static int OPERATION_CALCULATE = 0;     // 归零
+    public final static int OPERATION_AC = 0;// 等于号
 
     @FXML
-    public ListView listView;
-    @FXML
-    public GridPane girdPane;
-
-    private ArrayList<String> list = new ArrayList<>();
-    private ObservableList<String> listViewData = FXCollections.observableArrayList();
-
-    @FXML
-    public Button number; // .
+    public Button number;
     public Button number0;
     public Button number1;
     public Button number2;
@@ -61,50 +79,69 @@ public class CalculatorController extends HomeController {
     // 归零
     public Button operation1;
 
+    @FXML
+    public TextField show;
+    public ListView listView;
+    public GridPane girdPane;
+    private ArrayList<String> list = new ArrayList<>();
+    private ObservableList<String> listViewData = FXCollections.observableArrayList();
+
     public Control[] numberControls;
     public Control[] symbleControls;
     public Control[] operationControls;
 
+    // 定义状态
     public String[] symbles = new String[] { "求余", "乘方", "开根", "+", "-", "*", "/" };
-    public String[] numbers = new String[] { "", "", "" };
-    public int nowNumber = 1; // 0: 第一个数字 1：第二个数字
+    public String[] numbers = new String[] { "", "", ""};
+    public int nowNumber = 1; // 1: 第一个数字 2：第二个数字
     public boolean hasPoint = false; // 是否有小数点
     public boolean hasSymble = false; // 是否有加减乘除的符号
     public boolean hasNumber = false; // 是否有数字
     public int symble = 0;
-    String result = "";
+    public String result = "";
 
     @FXML
     public void initialize() {
         super.initialize();
 
         // 循环添加事件
-        numberControls = new Control[] { number, number0, number1, number2, number3, number4, number5, number6, number7,
+        numberControls = new Control[] {
+                number, number0, number1,
+                number2, number3, number4,
+                number5, number6, number7,
                 number8, number9 };
-        symbleControls = new Control[] { symble0, symble1, symble2, symble3, symble4, symble5, symble6 };
+
+        symbleControls = new Control[] {
+                symble0, symble1, symble2,
+                symble3, symble4, symble5,
+                symble6 };
+
         operationControls = new Control[] { operation0, operation1 };
 
-        for (int i = 0; i < 11; i++) {
+        // 数字键绑定监听事件
+        for (int i = NUMBER_Point; i <= NUMBER_9; i++) {
             final int finalI = i;
             numberControls[i].setOnMouseClicked(me -> {
-                pressNumber(finalI - 1);
+                pressNumber(finalI);
             });
         }
-        for (int i = 0; i <= 6; i++) {
+        // 运算符
+        for (int i = SYMBLE_REMAINDER; i <= SYMBLE_DIVIDE; i++) {
             final int finalI = i;
             symbleControls[i].setOnMouseClicked(me -> {
                 pressSymble(finalI);
             });
         }
+        // 指令
         operation0.setOnMouseClicked(me -> {
-            pressOperation(0);
+            pressOperation(OPERATION_CALCULATE);
         });
         operation1.setOnMouseClicked(me -> {
-            pressOperation(1);
+            pressOperation(OPERATION_AC);
         });
 
+        // 更新标签大小
         updateLabelSize();
-
         girdPane.widthProperty().addListener(me -> {
             updateLabelSize();
         });
@@ -129,21 +166,20 @@ public class CalculatorController extends HomeController {
     // 按下数字键
     public void pressNumber(int i) {
         if (hasPoint && hasNumber) {
-            if (i != -1) {
+            if (i != NUMBER_Point) {
                 numbers[nowNumber] += String.valueOf(i);
             }
         }
         if (!hasPoint && hasNumber) {
-            if (i != -1) {
+            if (i != NUMBER_Point) {
                 numbers[nowNumber] += String.valueOf(i);
-            }
-            if (i == -1) {
+            } else {
                 hasPoint = true;
                 numbers[nowNumber] += ".";
             }
         }
         if (!hasPoint && !hasNumber) {
-            if (i == -1) {
+            if (i != NUMBER_Point) {
                 hasPoint = true;
                 numbers[nowNumber] += ".";
             } else {
@@ -169,10 +205,10 @@ public class CalculatorController extends HomeController {
     // 按下等于和归零
     public void pressOperation(int i) {
         try { // 是否符合规范在这里捕获
-            if (i == 1) {
+            if (i == OPERATION_AC) {
                 init();
             }
-            if (i == 0) {
+            if (i == OPERATION_CALCULATE) {
                 calculator(); // 结果赋予result
                 String temp = result;
                 updateCache(); // 更新缓存
@@ -200,37 +236,42 @@ public class CalculatorController extends HomeController {
         }
     }
 
-    // 抛出自定义的异常
     private BigDecimal calculator(BigDecimal bd1, BigDecimal bd2, int i) throws InputException {
+        return calculator(bd1, bd2, i, 10);
+    }
+
+    private BigDecimal calculator(BigDecimal bd1, BigDecimal bd2, int i, int precision) throws InputException {
         BigDecimal res = null;
+        MathContext mc = new MathContext(precision);
         try {
             switch (i) {
-            case 0:
-                res = bd1.divideAndRemainder(bd2, new MathContext(10))[1];
+            case SYMBLE_REMAINDER:
+                res = bd1.divideAndRemainder(bd2,mc)[1];
                 break;
-            case 1:
-                res = bd1.pow(bd2.intValue(), new MathContext(10));
+            case SYMBLE_POW:
+                res = bd1.pow(bd2.intValue(), mc);
                 numbers[2] = String.valueOf(bd2.intValue());
                 break;
-            case 2:
-                res = new BigDecimal(Math.pow(bd1.doubleValue(), 1 / bd2.doubleValue()), new MathContext(10));
+            case SYMBLE_SQRT:
+                res = new BigDecimal(Math.pow(bd1.doubleValue(), 1 / bd2.doubleValue()), mc);
                 break;
-            case 3:
-                res = bd1.add(bd2, new MathContext(10));
+            case SYMBLE_ADD:
+                res = bd1.add(bd2, mc);
                 break;
-            case 4:
-                res = bd1.subtract(bd2, new MathContext(10));
+            case SYMBLE_SUBSTRUCT:
+                res = bd1.subtract(bd2, mc);
                 break;
-            case 5:
-                res = bd1.multiply(bd2, new MathContext(10));
+            case SYMBLE_MULTIPLY:
+                res = bd1.multiply(bd2, mc);
                 break;
-            case 6:
-                res = bd1.divide(bd2, 10, RoundingMode.HALF_UP);
+            case SYMBLE_DIVIDE:
+                res = bd1.divide(bd2, precision, RoundingMode.HALF_UP);
                 break;
             default:
                 res = bd1;
             }
         } catch (Exception e) {
+            // 抛出自定义的异常
             throw new InputException("输入错误");
         }
         return res;
